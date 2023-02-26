5f80cf255ea1a09aaf082ddfe079920e84a28278
Segimara <76587473+Segimara@users.noreply.github.com>
2023-02-27 01:53:26 +0200

    Create basic files structure

---
+++ MainService/Services/FileHandlerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainService.Services
+{
+    public class FileHandlerService
+    {
+        private 
+        public FileHandlerService()
+        {
+
+        }
+    }
+}
